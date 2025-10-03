fc691c35728372e2dac23fb6f8e6ab59680ad552
Chadchenko Viktor <chadchenko.vs@gmail.com>
2025-10-03 16:39:05 +0300

    work for errors

--- task_4.4.js
+++ task_4.4.js
@@ -1,16 +1,12 @@
-switch (numOrStr = prompt('Введите чайник или 123')) {
+const numOrStr = prompt('Input number or string');
+
+switch (numOrStr) {
     case null:
-        alert('Ви скасували');
+        console.log('Ви скасували');
         break;
     case '':
-        alert('Empty String');
+        console.log('Empty String');
         break;
-    case 'чайник':
-        alert('Вы заказали чайник');
-        break;
-    case '123':
-        alert('Вы ввели число');
-        break;
-    default:
-        alert('Ок! Это тоже вариант )');
-}
+    case numOrStr:
+        console.log('String'); 
+} 
