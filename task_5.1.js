ba30fb7fa2b343ac7517b778e7d9fb0a0cf074ce
Chadchenko Viktor <chadchenko.vs@gmail.com>
2025-10-06 14:34:57 +0300

    work for errors

--- task_5.1.js
+++ task_5.1.js
@@ -2,8 +2,10 @@
 let task = 'task № 5.1';
 console.log(task);
 
-let i = 20;
-while (i <= 30) {
-    console.log(i + ' ');
-    i += 0.5;
+let st = '';
+
+for (let i = 20; i <= 30; i += 0.5 ) {
+     st += ` ${i}`;
 }
+
+console.log(st);
