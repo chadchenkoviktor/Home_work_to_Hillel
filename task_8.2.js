4fa24c2c487fc71e46b2318d6396720b7011f83e
Chadchenko Viktor <chadchenko.vs@gmail.com>
2025-10-18 12:42:43 +0300

    home work №9

--- task_8.2.js
+++ task_8.2.js
@@ -1,10 +1,3 @@
 task = 'task № 8.2';
 console.log(task);
 
-function getCurry (a) {
-    return function (b) {
-        return a * b;
-    }
-}
-
-console.log(getCurry (3)(7));
--- task_8.3.js
+++ task_8.3.js
@@ -1,18 +1,4 @@
 task = 'task № 8.3';
 console.log(task);
 
-function getEnterNumber() {
-    let enter = +(prompt('Введите число '));
-
-    for (let i = 0; i < 10; i++) {
-       if (enter < 100 || enter === NaN) {
-            enter = +(prompt('Введите еще число '));
-        } else if (enter > 100) {
-            return console.log(`Вы ввели число ${enter}`)
-        } 
-    }
-
-    return console.log(`Цикл окончен окончен числом ${enter}`); ;
-}
-
-getEnterNumber();      
+      
